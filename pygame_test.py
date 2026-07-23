a3ec7e87b9b24a2641c1c6c0435d3ed2d9cecc74
Ivan <i.bukashin1@gmail.com>
2026-07-23 07:59:52 +0300

    Update pygame_test.py

--- pygame_test.py
+++ pygame_test.py
@@ -32,24 +32,3 @@ while running:
 
     # Отобразить нарисованные элементы в окне
     pygame.display.update()
-
-
-# Деинициализирует все модули Pygame, которые были инициализированы ранее
-pygame.quit()
-
-from math import floor
-
-def find_max_even_number(numbers):
-    """
-    Ищет максимальное чётное значение в списке положительных целых значений, переданном в параметр функции.
-    """
-    CurrentMax = 0
-    for number in numbers:
-        if number % 2 == 0: CurrentMax = max(number, CurrentMax)
-    return CurrentMax
-
-max_even = find_max_even_number([1, 2, 3, 4, 5])
-# Попробуйте передать в find_max_even_number() другие списки:
-# [10, 8, 6, 4, 2]
-# [2, 12, 85, 0, 6]
-print(f'Максимальное чётное число: {max_even}')
